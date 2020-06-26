cf71fd17c30250a7d8fd0ff7176019c9896536aa
feriferdinan <feri.ferdinan79@gmail.com>
2020-06-26 09:29:55 +0700

    update

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,10 @@ var AuthController = require('../controllers/AuthController');
 
 
 /* GET users listing. */
+
+router.get('/tes', function (req, res) {
+    res.send({ message: "Tes OK" })
+});
 router.post('/login', AuthController.login);
 // router.post('/register', AuthController.register);
 // router.get('/verify', AuthController.verifyEmail);
