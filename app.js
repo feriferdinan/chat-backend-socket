f0fe50c2acac4c2e9c5a1015135bff860733d59d
feriferdinan <feri.ferdinan79@gmail.com>
2020-06-28 15:44:59 +0700

    update

--- app.js
+++ app.js
@@ -39,9 +39,6 @@ app.use(cors({
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', function (req, res) {
-  res.send({ message: "CHAT YUK" })
-});
 app.use('/auth', authRouter);
 app.use(middleware.auth);
 app.use('/user', usersRouter);
